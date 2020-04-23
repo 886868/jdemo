5fc67410ff6c3f6583e12d067511e29612e9c0b8
ymr <yueminrui@sis.sh.cn>
2020-04-23 16:46:55 +0800

    cccc

--- src/main/java/com/mrle/jdemo/JdemoApplication.java
+++ src/main/java/com/mrle/jdemo/JdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JdemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(JdemoApplication.class, args);
 
 	}
